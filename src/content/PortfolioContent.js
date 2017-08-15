1bf9df80fe45ca3fd753c598540c655aeb9ce69d
Henry Angulo <hmac1994@gmail.com>
2017-08-15 02:57:06 -0400

    Traducción pendiente del portafolio y cambio de un icono.

--- src/content/PortfolioContent.js
+++ src/content/PortfolioContent.js
@@ -41,6 +41,90 @@ const portfolioInfo = {
             text: 'App web sencilla, usando html, css (bootstrap) y jquery para generar citas de forma aleatoria, y ofreciendo al usuario la opción de elegir categorias.'
         },
     ],
+    'EN': [
+        {
+            name: 'Mapa interactivo CDCVE',
+            src: './img/portafolio/cdcve.jpg',
+            link: 'http://cdcve.es/nosotros/',
+            text: 'In charge of the front-end for the interactive map of Venezuela made for the CDCVE (center of the Cultural Diversity in Venezuela) in Spain. It divides the country into its regions and show you its history, culture and traditions. A real informative experience and rich in multimedia content, in addition to having a beautiful and fully responsive design.'
+        },
+        {
+            name: 'Marcela Prieto',
+            src: './img/portafolio/marcela.jpg',
+            link: 'http://marcelaprieto.com/',
+            text: 'Development of the personal page, available in Spanish and English, of Marcela Prieto, Colombian activist and journalist, director of Foros Semana, an important medium of the country, vice-president of the Liberal Network of Latin America and member of the Editorial committee of Americas Quarterly.'
+        },
+        {
+            name: 'Ciurlizza Albumes',
+            src: './img/portafolio/ciurliza.jpg',
+            link: 'http://www.ciurlizzalbumesperu.com/',
+            text: 'Development of the page for a great albums store based in Peru, with its business information, product catalog and contact.'
+        },
+        {
+            name: 'ConFusion',
+            src: './img/portafolio/ciurliza.jpg',
+            text: 'Application for the Full Stack specialization of the Hong Kong University of Technology. It consists of a Single page application (SPA) Web made with angular and Bootstrap, a mobile app made with Ionic, angular and Cordova, both consuming the API made in NodeJS (a version with Express and another with Loopback) and MongoDB for the completion of specialization.'
+        },
+        {
+            name: 'Memegram',
+            src: './img/portafolio/memegram.jpg',
+            text: 'Web application made in Node and Express, with MongoDB and moongose for the database, Redis for the sessions, Socket.io and Handlebars JS for the real-time updating of the images. This App uses Node to make the server-side rendering with Jade (HTML) for views. '
+        },
+        {
+            name: 'Weather App',
+            src: './img/portafolio/weather.jpg',
+            link: 'https://codepen.io/HenryMAC/pen/aZpXLQ',
+            text: 'Simple Web App using HTML, CSS (bootstrap) and jquery to show the climate (Openweather API) to the user based on its location (obtained with an IP-based API), giving options for unit conversion.'
+        },
+        {
+            name: 'Random Quotes App',
+            src: './img/portafolio/quotes.jpg',
+            link: 'https://codepen.io/HenryMAC/full/VjwoBB/',
+            text: 'Simple Web App, using HTML, CSS (bootstrap) and jquery to generate quotations in a random way, and offering the user the option to choose Categories.'
+        },
+    ],
+    'FR': [
+        {
+            name: 'Mapa interactivo CDCVE',
+            src: './img/portafolio/cdcve.jpg',
+            link: 'http://cdcve.es/nosotros/',
+            text: "En charge de le développement front-end pour la carte interactive du Venezuela, faite pour le CDCVE (Centre de la diversité culturelle du Venezuela) en Espagne. Il divise le pays en ses régions et vous montrer son histoire, sa culture et ses traditions. Une expérience très instructive et riche en contenu multimédia, en plus d'avoir un beau design pleinement responsive."
+        },
+        {
+            name: 'Marcela Prieto',
+            src: './img/portafolio/marcela.jpg',
+            link: 'http://marcelaprieto.com/',
+            text: "Développement de la page personnelle, disponible en espagnol et en anglais, de Marcela Prieto, activiste et journaliste colombienne, directrice de Foros Semana, un important médium du pays, vice-présidente du réseau libéral d'Amérique latine et membre du Comité éditorial de Americas Quarterly."
+        },
+        {
+            name: 'Ciurlizza Albumes',
+            src: './img/portafolio/ciurliza.jpg',
+            link: 'http://www.ciurlizzalbumesperu.com/',
+            text: "Développement de la page pour un grand magasin d'albums basé au Pérou, avec ses informations d'affaires, catalogue de produits et contact."
+        },
+        {
+            name: 'ConFusion',
+            src: './img/portafolio/ciurliza.jpg',
+            text: "App pour la spécialisation complète a l'Université de technologie de Hong Kong. Il se compose d'une Sigle page app (Spa) Web fait avec angular et bootstrap, une app mobile faite avec Ionic, angular et Cordova, cette deux en consommant de l'API faite dans NodeJS (une version avec Express et un autre avec Loopback) et MongoDB pour l'achèvement de la spécialisation."
+        },
+        {
+            name: 'Memegram',
+            src: './img/portafolio/memegram.jpg',
+            text: "Application Web faite avec Node et Express, avec MongoDB et moongose pour la base de données, redis pour les sessions, Socket.IO et handlebars.js pour la mise à jour en temps réel des images. Cette application utilise Node avec Jade (html) pour le rendu de les vues."
+        },
+        {
+            name: 'Weather App',
+            src: './img/portafolio/weather.jpg',
+            link: 'https://codepen.io/HenryMAC/pen/aZpXLQ',
+            text: "Simple Web App en utilisant HTML, CSS (bootstrap) et jQuery pour montrer le climat (Openweather API) à l'utilisateur en fonction de son emplacement (obtenu avec une API basée sur l'IP), donnant des options pour la conversion d'unité."
+        },
+        {
+            name: 'Random Quotes App',
+            src: './img/portafolio/quotes.jpg',
+            link: 'https://codepen.io/HenryMAC/full/VjwoBB/',
+            text: "Simple Web App, en utilisant HTML, CSS (bootstrap) et jQuery pour générer des citations d'une manière aléatoire, et offrant à l'utilisateur la possibilité de choisir des catégories."
+        },
+    ],
 }
 
 export default portfolioInfo;
